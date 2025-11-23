56fe22a664360254852ff178dd50bfc2b5d0a86d
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-23 17:36:56 +0200

    refactor(solid3): remove Square-Rectangle inheritance (fix LSP)

---
+++ task3/Solid_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+interface IShape
+{
+    int GetArea();
+}
+
+class Rectangle : IShape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Square : IShape
+{
+    public int Side { get; set; }
+
+    public int GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IShape rect = new Rectangle { Width = 5, Height = 10 };
+        Console.WriteLine(rect.GetArea());   // 50
+
+        IShape sq = new Square { Side = 10 };
+        Console.WriteLine(sq.GetArea());     // 100
+
+        Console.ReadKey();
+    }
+}
